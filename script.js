549353c47511154020e416014d72c771e9f4accb
tanaboon123 <tanaboon.sa@ku.th>
2025-05-28 22:50:55 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("เว็บไซต์โหลดแล้ว!");
+const btnHome = document.getElementById('btn-home');
+const btnPortfolio = document.getElementById('btn-portfolio');
+
+const homePage = document.getElementById('home-page');
+const portfolioPage = document.getElementById('portfolio-page');
+
+btnHome.addEventListener('click', () => {
+  homePage.classList.add('active');
+  portfolioPage.classList.remove('active');
+});
+
+btnPortfolio.addEventListener('click', () => {
+  portfolioPage.classList.add('active');
+  homePage.classList.remove('active');
+});
+
